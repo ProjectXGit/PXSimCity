771e2e231dd8ccc4b22b9f325e7e8a4c436d25b7
Iclipsee <iclipskii@gmail.com>
2019-11-13 15:53:49 +0100

    First commit of PXSimCity

---
+++ src/net/projectx/simcity/main/Data.java
@@ -0,0 +1,10 @@
+package net.projectx.simcity.main;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * ~Yannick on 12.11.2019 at 21:43 o´ clock
+ */
+public class Data {
+    public static Plugin instance;
+}
