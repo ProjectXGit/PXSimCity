8977ab0216ecf5d8d9c1f44309fd01eee49e0f23
Iclipsee <iclipskii@gmail.com>
2019-11-13 16:04:07 +0100

    Repaired Version of all

--- src/net/projectx/simcity/main/Main.java
+++ src/net/projectx/simcity/main/Main.java
@@ -1,12 +1,9 @@
 package net.projectx.simcity.main;
 
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import sun.plugin2.liveconnect.BrowserSideObject;
-import sun.plugin2.main.server.Plugin;
 
-import java.net.PasswordAuthentication;
-import java.net.URL;
 
 /**
  * ~Yannick on 12.11.2019 at 21:42 o´ clock
@@ -26,15 +23,12 @@ public class Main extends JavaPlugin implements Plugin {
     }
 
     @Override
-
-
-
     public void onDisable() {
 
     }
 
     public static void registerCommands() {
-        //*LOOOOOOOOOOOOOOOOL
+        //*LOOOOOOOOOOOOOOOOL MIT MEHR OOOOOOOOOO
     }
 
     public static void registerListener() {
