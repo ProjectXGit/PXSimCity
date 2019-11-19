b8acb876b4ccc84fa0fc48d0b283c4ce384866ae
Iclipsee <iclipskii@gmail.com>
2019-11-19 16:04:22 +0100

    Every Wood Material is now working

--- src/net/projectx/simcity/functions/berufe/Foerster.java
+++ src/net/projectx/simcity/functions/berufe/Foerster.java
@@ -1,7 +1,6 @@
 package net.projectx.simcity.functions.berufe;
 
 import net.projectx.simcity.functions.mysql.MySQL_User;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,13 +13,11 @@ public class Foerster implements Listener {
 
     @EventHandler
     public void TreeChop(BlockBreakEvent event) {
-        System.out.println("Wird ausgeführt!");
         Block wood = event.getBlock();
         UUID uuid = event.getPlayer().getUniqueId();
         Random random = new Random();
-        System.out.println(wood.getBlockData());
-        if (wood.getType().equals(Material.OAK_LOG)) {
-            System.out.println("Legacy Log!");
+        System.out.println(wood.getType().toString());
+        if (wood.getType().toString().endsWith("_LOG")) {
             if(!MySQL_User.getJob(uuid).equalsIgnoreCase("Foerster")){
                 if (random.nextInt(5) == 0) {
                     System.out.println(random.nextInt(5));
